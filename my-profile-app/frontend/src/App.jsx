555f19e662f3869b31c0581199d5570b39c8fe4e
Niña Arcel R. Ramos <nrramos@student.apc.edu.ph>
2026-02-20 00:58:45 +0000

    edit app.jsx

--- my-profile-app/frontend/src/App.jsx
+++ my-profile-app/frontend/src/App.jsx
@@ -38,8 +38,6 @@ function App() {
     <div className="container">
       <h1 className="Cal-text">Guestbook</h1>
       
-      <p>Please fill out the form below to let us know what you think.</p>
-
       <div id="app">
         <form onSubmit={handleSubmit}>
           <p>
